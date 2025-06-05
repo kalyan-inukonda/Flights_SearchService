fd444d3b61224e1378f4ac66da7b75dd9098fe86
ivvskalyan <ivvskalyan1908@gmail.com>
2025-06-05 20:11:18 +0530

    added new table

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const CityRepository = require('./repository/city-repository');
 
 const setupAndstartServer = async () => {
 
     const app = express();
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: True}));
+    app.use(bodyParser.urlencoded({extended: true}));
+    
     app.listen(PORT,() => {
         console.log(`server started at ${PORT}`);
+        const obj = new CityRepository();
+        obj.createCity({name:"New Delhi"});
     });
 };
 
