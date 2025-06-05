7bc536f24abef5f41789053bd6ed2abb27311add
ivvskalyan <ivvskalyan1908@gmail.com>
2025-06-05 20:59:00 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,6 +6,7 @@ class CityRepository{
             const city = await City.create({name});
             return city;
         } catch(error){
+            console.log("something went wrong in the repository layer");
             throw {error};
         }
     }
@@ -17,10 +18,39 @@ class CityRepository{
                     id: cityId
                 }
             });
+            return true;
         } catch(error){
+            console.log("something went wrong in the repository layer");
             throw {error};
         }
     }
+
+    async updateCity(cityId,data){
+        try{
+            const city = await City.update(data,{
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch(error){
+            console.log("something went wrong in the repository layer");
+            throw {error};
+        }
+
+    }
+
+    async getCity(cityID){
+        try{
+            const city = await City.findByPk(cityID);
+            return city;
+        } catch(error){
+            console.log("something went wrong in the repository layer");
+            throw {error};
+        }
+
+    }
+
 }
 
 module.exports = CityRepository;
